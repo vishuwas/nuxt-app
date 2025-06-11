e128020e7f0ef4c94f97b186b1a2be4e5d1d1048
Vishwas Kataria <vishwas.kataria@stockjarvis.com>
2025-06-11 07:26:50 +0530

    Add dynamic post detail view

---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">{{ post?.title }}</h1>
+    <p class="mt-4">{{ post?.body }}</p>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: post } = await useFetch(
+  `https://dummyjson.com/posts/${route.params.id}`
+);
+</script>
